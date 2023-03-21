280648e30d6efa96b5b85c60837b5b357d333c02
Delvon Forrester <delvon@live.co.uk>
2023-03-21 13:39:01 +0000

    Adding task

---
+++ db/tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+$tasks = [
+    [
+        'classname' => 'theme_cul_moove\task\add_academicyear_custom_field',
+        'blocking' => '*',
+        'minute' => '0',
+        'hour' => '0',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    ]
+];
